00b05f0e0f110ecff856acd75a0080ec512c5bd7
stefanappdev <stevenbrown2dn@gmail.com>
2023-08-29 22:35:07 -0500

    Searchbar completed

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -3,20 +3,77 @@ import '../styles/App.css'
 import React,{ useEffect,useState } from 'react' 
 
 function Searchbar() {
+ 
+  const[SearchQuery,setSearchQuery]=useState("");
+  let [characters,setcharacters]=useState([]);
+  const[show,setshow]=useState(false);
+  let [names,setnames]=useState([])
+ 
+
+  const HandleChange=(e)=>{
+      setSearchQuery(e.target.value);
+      setshow(true);
+  }
+
+  const filterSearch=(input)=>{
+      if (SearchQuery.includes(input[0].toLowerCase())){ 
+        return true
+      }
+  }
+
+  const fetchCharacters=async(url)=>{
+      await fetch(url,{
+      method:'GET', 
+      'Content-Type':'application/json',
+    })
+    .then(resp=> resp.json())
+    .then(data=>{
+      
+      setcharacters(data.results);
+      let cnames=[]
+      
+
+      for (let x=0;x<data.results.length;x++){
+        cnames.push(data.results[x].name)
+      
+      }
+      console.log("names are:",cnames)
+      setnames(cnames);
+      console.log("Character names:",names)
+    })
+}
+
+  useEffect(()=>{
+    fetchCharacters("https://swapi.dev/api/people?/page=1")
+ .catch(err=>{
+   console.log('Something went wrong',err.message)
+ })
+ },[])
+
+console.log("characters array:",characters)
+console.log("names array:",names)
+let SearchResults=names.filter(filterSearch).map(name=>{
+      return(<p onClick={()=>{
+        
+                              setSearchQuery(name)
+                              setshow(false)  
+                            
+                            }}>{name}</p>)
+                          })
+
 
   return (
     <React.Fragment>
 
             <span>
-                <input type='text' placeholder="search..."/>
+                <input type='text' value={SearchQuery} onChange={HandleChange} placeholder="search..."/>
             </span>
                             
-            <div id="results-dropdown">
-                    <p>test</p>
-                    <p>test</p>
-                    <p>test</p>
-                
-            </div>
+           {show? <div id="results-dropdown">
+                    {SearchResults}
+                    </div>:
+                    
+                    <div></div>}
 
 
     </React.Fragment>
